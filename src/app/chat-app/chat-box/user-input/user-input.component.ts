811e7d48f5d9e3e965f92cd9c9525383f80b890b
nxsang <nxsang@axonactive.com>
2023-08-07 17:32:43 +0700

    almost done

--- src/app/chat-app/chat-box/user-input/user-input.component.ts
+++ src/app/chat-app/chat-box/user-input/user-input.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-input',
   templateUrl: './user-input.component.html',
-  styleUrls: ['./user-input.component.css']
+  styleUrls: ['./user-input.component.css'],
 })
 export class UserInputComponent {
+  backspaceEvent(e) {
+    const val = e.target.value;
+    const count = val.split('\n').length;
+    e.target.rows = count < 5 ? count : 5;
+    console.log(count);
+  }
 
+  enter(e) {
+    e.preventDefault();
+  }
 }
